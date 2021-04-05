191f9092254c9732543189b22ea6eced2e74f2ac
alexhstrickland <alexhstrickland@gmail.com>
2021-04-05 19:47:58 -0400

    adding routes

--- server.js
+++ server.js
@@ -2,3 +2,13 @@ const express = require('express');
 const mongojs = require('mongojs');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 4050;
+
+const app = express();
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static{'public'});
+app.use(require('./routes/htmlRoutes'));
+app.use(require('./routes/apiRoutes'));
