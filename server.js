614e9b0ac3898e480c9315f6c35877fca2fdad1c
alexhstrickland <alexhstrickland@gmail.com>
2021-04-05 19:48:45 -0400

    fixwed typo

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ const mongoose = require('mongoose');
 const PORT = process.env.PORT || 4050;
 
 const app = express();
+
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static{'public'});
+app.use(express.static('public'));
 app.use(require('./routes/htmlRoutes'));
 app.use(require('./routes/apiRoutes'));
