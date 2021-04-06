16bd1165097fbcb44630cef0d802b85efad3e137
alexhstrickland <alexhstrickland@gmail.com>
2021-04-05 20:51:24 -0400

    added api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,24 +5,31 @@ const db = require('../models');
 router.get('/api/workouts', (req, res) => {
     db.Workout.find().then(function(results){
         res.json(results);
+        console.log(res.json);
     })
 });
 
 router.post('/api/workouts', (req, res) => {
-    db.Workout.find().then(function(results){
+    db.Workout.create(req.body).then(function(results){
         res.json(results);
+        console.log(res.json);
     })
 });
 
-router.put('/api/workouts', (req, res) => {
-    db.Workout.find().then(function(results){
+router.put('/api/workouts/:id', (req, res) => {
+    db.Workout.updateOne(
+        { _id: req.params.id },
+        { $push: { exercises: req.body } }
+    ).then(function(results){
         res.json(results);
+        console.log(res.json);
     })
 });
 
-router.get('/api/workouts', (req, res) => {
+router.get('/api/workouts/range', (req, res) => {
     db.Workout.find().then(function(results){
         res.json(results);
+        console.log(res.json);
     })
 });
 
