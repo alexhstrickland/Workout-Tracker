31dbc997d172d7e0203134dfdcdf6272dff394c9
alexhstrickland <alexhstrickland@gmail.com>
2021-04-07 21:51:52 -0400

    added functionality and also updated the README

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # Workout-Tracker
+
+  [![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)
+
+  ## Description
+  This is an application that allows a user to create and track their personal workouts. Users are able to log multiple workouts within a single day and track the name, type, weight, sets, reps, ad duration of the exercises.
+
+  ## Table of Contents
+
+  * [Description](#Description)
+  * [Installation](#Installation)
+  * [Usage](#Usage)
+  * [License](#License)
+  * [Questions](#Questions)
+
+  ## Installation
+
+  To install necessary dependencies, please run the following command:
+  ```
+  npm mongoose, npm morgan, npm express
+  ```
+
+  ## Usage
+  To use, the user will need to enter their workouts through the application and track these workouts in the dashboard. The dashboard can be accessed by clicking the link at the top of the page.
+
+  ## License
+  This project is covered under the ISC license. Visit the following link for more information on this license: [ISC](https://opensource.org/licenses/ISC)
+
+
+  ## Contributing
+  
+
+  ## Tests
+  To test, please run the following command:
+  ```
+  npm test
+  ```
+
+  ## Questions
+  For any questions about the project, please contact me by either of the following links:
+  
+  * Email - alexhstrickland@gmail.com 
+  
+  or visit my GithHub profile
+  
+  * GitHub - [alexhstrickland](https://github.com/alexhstrickland)
+
--- models/workout.js
+++ models/workout.js
@@ -3,43 +3,57 @@ const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
 
-    day: {
-        type: Date,
-        default: Date.now
-    },
-    exercises: [
-        {
-            type: {
-                type: String
-            },
-        
-            name: {
-                type: String
-            },
-        
-            distance: {
-                type: Number
-            },
-        
-            weight: {
-                type: Number
-            },
-        
-            sets: {
-                type: Number
-            },
+        day: {
+            type: Date,
+            default: () => new Date(),
+        },
+        exercises: [
+            {
+                type: {
+                    type: String,
+                },
+            
+                name: {
+                    type: String,
+                },
+            
+                distance: {
+                    type: Number,
+                },
+            
+                weight: {
+                    type: Number,
+                },
+            
+                sets: {
+                    type: Number,
+                },
 
-            reps: {
-                type: Number
-            },
+                reps: {
+                    type: Number,
+                },
 
-            duration: {
-                type: Number
+                duration: {
+                    type: Number,
+                }
             }
-        }
-    ]
-});
+        ],
+    },
+    {toJSON: {
+            virtuals: true
+        }});
+    WorkoutSchema.virtual('totalDuration')
+    .get(function () {
+        const duration = this.exercises.reduce((acc, curr) => {
+            return acc + curr.duration;
+        }, 0);
 
-const Workout = mongoose.modell("Workout", WorkoutSchema);
+        console.log(duration);
+
+        return duration;
+
+    });
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
 
 module.exports = Workout;
