8c7780abcc05288c1d9ad77f57386011a4d62865
alexhstrickland <alexhstrickland@gmail.com>
2021-04-08 09:14:34 -0400

    working to fix api

--- public/exercise.js
+++ public/exercise.js
@@ -19,7 +19,7 @@ let shouldNavigateAway = false;
 
 async function initExercise() {
   let workout;
-
+  console.log('init exercise')
   if (location.search.split("=")[1] === undefined) {
     workout = await API.createWorkout()
     console.log(workout)
