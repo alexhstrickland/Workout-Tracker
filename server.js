66d3399b90d12fc3725305d9ed3e3d8e9d8e4c43
alexhstrickland <alexhstrickland@gmail.com>
2021-04-08 10:23:09 -0400

    trying to fix crash

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
-const mongojs = require('mongojs');
+// const mongojs = require('mongojs');
 
 const PORT = process.env.PORT || 4050;
 
@@ -11,8 +11,8 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-app(require('./public/routes/htmlRoutes'));
-app(require('./public/routes/apiRoutes'));
+app(require('./routes/htmlRoutes'));
+app(require('./routes/apiRoutes'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
     useNewUrlParser: true,
