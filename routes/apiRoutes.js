2d778f6a2e54b922959dc257e1f9cb15fb4e89fe
alexhstrickland <alexhstrickland@gmail.com>
2021-04-08 10:04:43 -0400

    fixes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,63 +1,65 @@
 const express = require('express')
-const router = express.Router();
+// const router = express.Router();
 const mongojs = require('mongojs');
 const db = require('../models');
 
-router.get('/api/workouts', (req, res) => {
-    db.Workout.find({}, (error, data) => {
-        if (error) {
-            res.send(error);
-        } else {
-            res.json(data);
-        }
-    });
-});
+module.exports = function(app) {
 
-router.get("/api/workout/:id", (req, res) => {
-    db.Workout.findOne(
-        {
-            _id: mongojs.ObjectID(req.params.id)
-        },
-        (err, data) => {
-            if (err) {
-                res.send(err)
+    app.get('/api/workouts', (req, res) => {
+        db.Workout.find({}, (error, data) => {
+            if (error) {
+                res.send(error);
             } else {
-                res.send(data)
+                res.json(data);
             }
-        }
-    )
-});
-
-router.post('/api/workouts', (req, res) => {
-    db.Workout.create({}, (error, data) => {
-        if (error) {
-            res.send(error)
-        } else {
-            res.json(data)
-        }
+        });
     });
-});
 
-router.put('/api/workouts/:id', (req, res) => {
-    db.Workout.findByIdAndUpdate(
-        req.params.id,
-        { $push: { exercises: req.body } },
-        {new: true, runValidators: true }
-    ).then(data => res.json(data))
-        .catch(err => {
-            res.json(err)
-            console.log(res.json);
-    })
-});
-
-router.get('/api/workouts/range', (req, res) => {
-    db.Workout.find({}, (error, data) => {
-        if (error) {
-            res.send(error);
-        } else {
-            res.json(data);
-        }
+    app.get('/api/workout/:id', (req, res) => {
+        db.Workout.findOne(
+            {
+                _id: mongojs.ObjectID(req.params.id)
+            },
+            (err, data) => {
+                if (err) {
+                    res.send(err)
+                } else {
+                    res.send(data)
+                }
+            }
+        )
     });
-});
 
-module.exports = router;
+    app.post('/api/workouts', (req, res) => {
+        db.Workout.create({}, (error, data) => {
+            if (error) {
+                res.send(error)
+            } else {
+                res.json(data)
+            }
+        });
+    });
+
+    app.put('/api/workouts/:id', (req, res) => {
+        db.Workout.findByIdAndUpdate(
+            req.params.id,
+            { $push: { exercises: req.body } },
+            {new: true, runValidators: true }
+        ).then(data => res.json(data))
+            .catch(err => {
+                res.json(err)
+                console.log(res.json);
+        })
+    });
+
+    app.get('/api/workouts/range', (req, res) => {
+        db.Workout.find({}, (error, data) => {
+            if (error) {
+                res.send(error);
+            } else {
+                res.json(data);
+            }
+        });
+    });
+
+};
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,17 +1,19 @@
 const express = require('express');
-const router = express.Router();
+// const router = express.Router();
 const path = require('path')
 
-router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"))
-});
+module.exports = function(app) {
 
-router.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/stats.html"))
-});
+    app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, "../index.html"))
+    });
 
-router.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/exercise.html"))
-});
+    app.get("/stats", (req, res) => {
+        res.sendFile(path.join(__dirname, "../stats.html"))
+    });
 
-module.exports = router;
+    app.get("/exercise", (req, res) => {
+        res.sendFile(path.join(__dirname, "../exercise.html"))
+    });
+};
+
